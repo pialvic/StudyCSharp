47524d34a3419b57ce0f50b535ef13216ae18435
pialvic <pialvic@gmail.com>
2025-09-17 17:28:37 +0300

    Init: add .gitignore and clean VS artifacts

--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет Мир!!!");
-Console.WriteLine("Давай не будем унывать!");
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Простой калькулятор — шаги реализации: Add/Subtract/Multiply/Divide");
+
+        // TODO #1: Реализуй ввод чисел и операцию
+        //  - используй double.TryParse для безопасного ввода
+        //  - для начала можешь запросить 2 числа и одну операцию, затем вызвать нужный метод
+
+        // Пример вызова (реализуй сам):
+        // double a = ReadDouble("Введите первое число: ");
+        // double b = ReadDouble("Введите второе число: ");
+        // char op = ReadOperation("Введите операцию (+ - * /): ");
+        // double result = 0;
+        // switch (op) { case '+': result = Add(a, b); break; ... }
+        // Console.WriteLine($"Результат: {result}");
+
+        // TODO #2: Добавь меню/цикл (позже)
+    }
+
+    // TODO #A: реализуй методы ниже по очереди
+    public static double Add(double a, double b)
+    {
+        throw new NotImplementedException(); // реализуй и коммить
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static double Divide(double a, double b)
+    {
+        // Обязательная проверка деления на ноль
+        throw new NotImplementedException();
+    }
+
+    // Подсказка: можешь добавить вспомогательные методы для ввода:
+    // static double ReadDouble(string prompt) { ... }
+    // static char ReadOperation(string prompt) { ... }
+}
